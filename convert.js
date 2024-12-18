38265b896cf02f7b6bfcc166a2ad31f6150d1b8f
Olivia <olivia.guyot@camptocamp.com>
2024-12-18 10:02:30 +0100

    update

--- convert.js
+++ convert.js
@@ -50,43 +50,176 @@ function analyzeStyle(mbStyle) {
   );
 }
 
-/**
- * @param {string} sourceLayer
- * @param {Object} mbStyle JSON style
- * @return {Object}
- */
-function getStyleForSourceLayer(sourceLayer, mbStyle) {
-  const layers = mbStyle.layers
-    .filter((l) => l["source-layer"] === sourceLayer)
-    .filter((l) => l?.layout?.visibility === "visible");
-  const filters = collect(layers, "filter");
-  const filter = filters.length
-    ? simplifyExpression(["any", ...filters])
-    : undefined;
-  if (!layers[0]) {
-    return { filter };
+function fixExpression(expression) {
+  if (!Array.isArray(expression)) return expression;
+  if (expression[0] === "all" && expression.length === 2) {
+    return fixExpression(expression[1]);
   }
-  return {
-    filter,
-    ...layers[0].paint,
-    ...layers[0].layout,
-  };
+  if (expression[0] === "get" && expression[1] === "$type") {
+    return ["geometry-type"];
+  }
+  if (expression[0] === "==") {
+    let compared = expression[1];
+    if (typeof compared === "string") compared = ["get", expression[1]];
+    return [
+      expression[0],
+      fixExpression(compared),
+      ...expression.slice(2).map(fixExpression),
+    ];
+  }
+  if (expression[0] === "interpolate") {
+    return [
+      expression[0],
+      expression[1],
+      ...expression.slice(2).map(fixExpression),
+    ];
+  }
+  return [expression[0], ...expression.slice(1).map(fixExpression)];
 }
 
 /**
- * @param {Object} mbStyle JSON style
+ * @param {string} layer
+ * @param {Object} spritesheet
+ * @param {string} spritesPath
+ * @return {Object}
  */
-export function convertToFlatStyle(mbStyle) {
-  analyzeStyle(mbStyle);
+function getStyleForSourceLayer(layer, spritesheet, spritesPath) {
+  const filters = [["==", ["get", "layer"], layer["source-layer"]]];
+  if (layer.filter) {
+    filters.push(fixExpression(layer.filter));
+  }
+  // convert zoom to web mercator resolution
+  if (layer.maxzoom) {
+    filters.push([
+      ">",
+      ["resolution"],
+      156543.03392804097 / Math.pow(2, layer.maxzoom),
+    ]);
+  }
+  if (layer.minzoom) {
+    filters.push([
+      "<",
+      ["resolution"],
+      156543.03392804097 / Math.pow(2, layer.minzoom),
+    ]);
+  }
 
-  const layers = mbStyle.layers;
-  const sourceLayers = collect(layers, "source-layer");
+  const style = {};
+  const props = { ...layer.paint, ...layer.layout };
+  for (const prop in props) {
+    const value = fixExpression(props[prop]);
+    switch (prop) {
+      case "fill-outline-color":
+        style["stroke-color"] = value;
+        style["stroke-width"] = style["stroke-width"] || 1;
+        break;
+      case "fill-color":
+        style["fill-color"] = value;
+        break;
+      case "fill-pattern": {
+        const spriteInfo = spritesheet[value];
+        style["fill-pattern-src"] = spritesPath;
+        style["fill-pattern-offset"] = [spriteInfo.x, spriteInfo.y];
+        style["fill-pattern-size"] = [spriteInfo.width, spriteInfo.height];
+        style["fill-pattern-scale"] = 64 / spriteInfo.width;
+        break;
+      }
+      case "line-color":
+        style["stroke-color"] = value;
+        break;
+      case "line-width":
+        style["stroke-width"] = value;
+        break;
+      case "line-cap":
+        style["stroke-line-cap"] = value;
+        break;
+      case "line-join":
+        style["stroke-line-join"] = value;
+        break;
+      case "line-offset":
+        style["stroke-offset"] = ["/", value, 2]; // FIXME: width in OL shaders is doubled! this is a bug
+        break;
+      case "icon-image": {
+        const spriteInfo = spritesheet[value];
+        if (props["symbol-placement"] === "point") {
+          style["icon-src"] = spritesPath;
+          style["icon-offset"] = [spriteInfo.x, spriteInfo.y];
+          style["icon-size"] = [spriteInfo.width, spriteInfo.height];
+          style["icon-scale"] = fixExpression(props["icon-size"]);
+        } else if (props["symbol-placement"] === "line") {
+          style["stroke-pattern-src"] = spritesPath;
+          style["stroke-pattern-offset"] = [spriteInfo.x, spriteInfo.y];
+          style["stroke-pattern-size"] = [spriteInfo.width, spriteInfo.height];
+          style["stroke-width"] = [
+            "*",
+            fixExpression(props["icon-size"]),
+            spriteInfo.height * 0.5,
+          ];
+        }
+        break;
+      }
+      case "symbol-spacing":
+        style["stroke-pattern-spacing"] = ["*", 0.5, value];
+        break;
+    }
+  }
 
-  return sourceLayers.reduce(
-    (prev, curr) => ({
-      ...prev,
-      [curr]: getStyleForSourceLayer(curr, mbStyle),
-    }),
-    {}
+  return {
+    filter: filters.length === 1 ? filters[0] : ["all", ...filters],
+    ...style,
+  };
+
+  // const layers = mbStyle.layers
+  //   .filter((l) => l["source-layer"] === sourceLayer)
+  //   .filter((l) => l?.layout?.visibility === "visible");
+  // const filters = collect(layers, "filter");
+  // const filter = filters.length
+  //   ? simplifyExpression(["any", ...filters])
+  //   : undefined;
+  // if (!layers[0]) {
+  //   return { filter };
+  // }
+  // return {
+  //   filter,
+  //   ...layers[0].paint,
+  //   ...layers[0].layout,
+  // };
+}
+
+// /**
+//  * @param {Object} mbStyle JSON style
+//  */
+// export function convertToFlatStyle(mbStyle) {
+// analyzeStyle(mbStyle);
+//
+// const layers = mbStyle.layers;
+// const sourceLayers = collect(layers, "source-layer");
+//
+// return sourceLayers.reduce(
+//   (prev, curr) => ({
+//     ...prev,
+//     [curr]: getStyleForSourceLayer(curr, mbStyle),
+//   }),
+//   {}
+//   );
+// }
+
+/**
+ * @param {string} stylePath JSON style
+ * @return {import('ol/style/flat').default}
+ */
+export async function convertToFlatStyle(stylePath) {
+  const base = await fetch(stylePath).then((resp) => resp.json());
+  const spritesheet = await fetch(`${base.sprite}.json`).then((resp) =>
+    resp.json()
   );
+  const spritesPath = `${base.sprite}.png`;
+
+  analyzeStyle(base);
+
+  return base.layers
+    .filter(
+      (layer) => layer.layout?.visibility !== "none" && layer.type !== "raster"
+    )
+    .map((layer) => getStyleForSourceLayer(layer, spritesheet, spritesPath));
 }
